343d3f5c1abf5cc8bc7330610c63092dec3475e5
Renato Vicente <rntvicente@gmail.com>
2021-04-03 10:27:27 -0300

    test: ensure SignUpController return an error when no name is provided

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -3,16 +3,18 @@ import { SingUpController } from './signup'
 describe('#SignUp Controller', () => {
   test('Should return 400 when no name is provided', () => {
     const sut = new SingUpController()
+    const expectedBody = new Error('Missing param: name')
 
     const httpRequest = {
       body: {
-        email: '',
-        password: '',
-        passwordConfirmation: ''
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
       }
     }
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(expectedBody)
   })
 })
