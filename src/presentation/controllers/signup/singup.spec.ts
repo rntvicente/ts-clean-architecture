b6f3300a78e69161ee496bb38f1c380ba2311bbe
Renato Vicente <rntvicente@gmail.com>
2021-04-03 21:03:56 -0300

    test: create helper created

--- src/presentation/controllers/signup/singup.spec.ts
+++ src/presentation/controllers/signup/singup.spec.ts
@@ -245,7 +245,7 @@ describe('#SignUp Controller', () => {
 
     const httpResponse = sut.handle(httpRequest)
 
-    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.statusCode).toBe(201)
     expect(httpResponse.body).toEqual({
       id: 'valid_id',
       email: 'valid_email@mail.com',
