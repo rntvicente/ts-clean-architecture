a8b57b7ed3114812e952f2c2c957a96c675aa630
Renato Vicente <rntvicente@gmail.com>
2021-04-12 22:16:41 -0300

    refactor: ensure DbAddAccount returns an Account on success

--- src/data/usescases/add-account/db-add-account.ts
+++ src/data/usescases/add-account/db-add-account.ts
@@ -19,6 +19,6 @@ export class DbAddAccount implements AddAccount {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
 
-    return await new Promise(resolve => resolve(account))
+    return account
   }
 }
