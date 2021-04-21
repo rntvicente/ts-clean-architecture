7401d9dbabf841916203531f0a02d5c13fcfdb2e
Renato Vicente <rntvicente@gmail.com>
2021-04-21 19:47:30 -0300

    test: ensure returns xml content type when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -12,4 +12,15 @@ describe('Content Type Middlaware', () => {
       .get('/content_type_json')
       .expect('content-type', /json/)
   })
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
